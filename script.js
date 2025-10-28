e112bf2798708dc2b2368496e532893f6b8a25d1
sadpotato56 <125994590+sadpotato56@users.noreply.github.com>
2025-10-28 23:04:21 +0700

    Add files via upload
    js và html update cùng với css mới

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   console.log('Script loaded');
-//   const hamburger = document.querySelector('.hamburger');
-//   const navLinks = document.querySelector('.nav-links');
-
-//   if (hamburger && navLinks) {
-//     hamburger.addEventListener('click', () => {
-//       navLinks.classList.toggle('open');
-//       hamburger.classList.toggle('open');
-//     });
-//   } else {
-//     console.error('Elements not found. Check if .hamburger and .nav-links exist in HTML.');
-//   }
-// });
-
-
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.hamburger');
   const navLinks = document.querySelector('.nav-links');
@@ -24,11 +7,21 @@ document.addEventListener('DOMContentLoaded', () => {
     navLinks.classList.toggle('open');
   });
 
-  // Tự động đóng menu khi bấm vào link
   document.querySelectorAll('.nav-links a').forEach(link => {
     link.addEventListener('click', () => {
       hamburger.classList.remove('open');
       navLinks.classList.remove('open');
     });
   });
+});
+
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $(".header").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".header").removeClass("active");
+        }
+    });
 });
